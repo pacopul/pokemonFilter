3f92e86bd357552dd32d7298b91bce5108ee5a6e
pacopul <profesor.pacopul@iesazarquiel.es>
2025-09-28 21:16:17 +0200

    builddocs

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+    // Ambas líneas añadidas:
+  // documentroot del proyecto
+  base: '/pokemonFilter/',
+  // Carpeta de salida configurada como 'docs'
+  // para poder alojar en GitHub Pages (main branch, docs folder)
+  build: {
+    outDir: 'docs',
+  },
+
 })
