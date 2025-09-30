d8ffa27870101de569e1b64879d5d14c5fe75038
pacopul <profesor.pacopul@iesazarquiel.es>
2025-09-30 19:48:13 +0200

    cssfile

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,37 +1,26 @@
 import { FaSearch } from "react-icons/fa";
+import './Search.css';
 
 const Search = ({ onSearch }) => {
-    /* Estilos en línea para el input y el icon */
-    const inputStyle = {
-        width: "100%",
-        fontSize: "1.3rem",
-        padding: "8px",
-        border: "none",
-    };
-    const iconStyle = {
-        fontSize: "2rem",
-        color: "#888",
-        marginRight: "8px"
-    };
     /* Maneja el cambio en el input 
     y llama a onSearch que es la función 
     que recibe el componente como parámetro
-    para pasarle el valor del input */
+    para pasarle el valor del input (query) */
 
     const handleInputChange = (e) => {
         onSearch(e.target.value);
     };
 
     return (
-        <div className="d-flex align-items-center border rounded-pill px-3 py-1">
-            <span style={iconStyle}>
+        <div class="d-flex align-items-center border rounded-pill px-3 py-1">
+            <span className="icon">
                 <FaSearch />
             </span>
             <input
                 type="search"
                 placeholder="Buscar..."
                 onChange={handleInputChange}
-                style={inputStyle}
+                className="boxsearch"
             />
         </div>
     );
